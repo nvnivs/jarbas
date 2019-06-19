8a3d559747d9a53bc1006ad3331f23e3e38190d2
n p <nvnivs@gmail.com>
2019-06-19 22:34:09 +0200

    Disabling enabling sudo password prompts

---
+++ cookbooks/jrb_workstation/recipes/osx_sudoers.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: jrb_workstation
+# Recipe:: osx_sudoers_nopass
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+#
+# Description: Disables pass for root user on sudoers to enable non-interactive commands in chef
+
+cookbook_file '/etc/sudoers' do
+  source 'sudoers'
+  owner 'root'
+  group 'wheel'
+  mode '0440'
+end
--- roles/workstation.rb
+++ roles/workstation.rb
@@ -1,16 +1,20 @@
 name 'workstation'
 description 'Provisions and configures a workstation'
 
-base_run_list = [
+common_run_list = [
   'recipe[jrb_workstation]'
 ]
 
-run_list(base_run_list)
+run_list(common_run_list)
 
 env_run_lists(
   '_default' => [],
-  'macbook' => base_run_list + [
-    'recipe[jrb_workstation::brew]',
-    'recipe[jrb_workstation::brew_cask]'
-  ]
+  'macbook' => [
+    'recipe[jrb_workstation::osx_sudoers_nopass]' ] +
+    common_run_list +
+    [
+      'recipe[jrb_workstation::brew]',
+      'recipe[jrb_workstation::brew_cask]',
+      'recipe[jrb_workstation::osx_sudoers]'
+    ]
 )
