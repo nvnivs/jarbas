8c686d2b123809ddddd8851d37cd1cc3d47593e2
n p <nvnivs@gmail.com>
2019-07-03 18:07:45 +0200

    Moved brew packages to attributes

---
+++ cookbooks/jrb_workstation/attributes/brew.rb
@@ -0,0 +1,21 @@
+default['jrb_workstation']['brew']['packages'] = [
+  'ansible',         # Automate deployment, configuration, and upgrading
+  'autossh',         # Automatically restart SSH sessions and tunnels
+  'coreutils',       # GNU File, Shell, and Text utilities
+  'git',             # Distributed revision control system
+  'golang',          # Open source programming language to build simple/reliable/efficient software
+  'jq',              # Lightweight and flexible command-line JSON processor
+  'lynx',            # Text-based web browser
+  'kubernetes-helm', # The Kubernetes package manager
+  'mtr',             # 'traceroute' and 'ping' in a single tool
+  'pandoc',          # Swiss-army knife of markup format conversion
+  'pass',            # Password manager
+  'python',          # Interpreted, interactive, object-oriented programming language
+  'python3',         # Interpreted, interactive, object-oriented programming language
+  'portaudio',       # Required for libspotify
+  'qemu',            # x86 and PowerPC Emulator
+  'ranger',          # File browser
+  'telnet',          # User interface to the TELNET protocol (built from macOS Sierra sources)
+  'tree',            # Display directories as trees (with optional color/HTML output)
+  'watch',           # Executes a program periodically, showing output fullscreen
+]
