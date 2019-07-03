1971201093574c8c4b8e0c7cbe467367bfa63981
n p <nvnivs@gmail.com>
2019-07-03 18:15:51 +0200

    Moved runlist from role to default recipe

--- cookbooks/jrb_workstation/recipes/default.rb
+++ cookbooks/jrb_workstation/recipes/default.rb
@@ -4,6 +4,10 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
+include_recipe 'jrb_workstation::osx_sudoers_nopass' if node['platform'] == 'mac_os_x'
+include_recipe 'jrb_workstation::brew' if node['platform'] == 'mac_os_x'
+include_recipe 'jrb_workstation::brew_cask' if node['platform'] == 'mac_os_x'
 include_recipe 'jrb_workstation::vim'
 include_recipe 'jrb_workstation::ruby'
 include_recipe 'jrb_workstation::vagrant'
+include_recipe 'jrb_workstation::osx_sudoers' if node['platform'] == 'mac_os_x'
--- roles/workstation.rb
+++ roles/workstation.rb
@@ -1,20 +1,4 @@
 name 'workstation'
 description 'Provisions and configures a workstation'
 
-common_run_list = [
-  'recipe[jrb_workstation]'
-]
-
-run_list(common_run_list)
-
-env_run_lists(
-  '_default' => [],
-  'macbook' => [
-    'recipe[jrb_workstation::osx_sudoers_nopass]' ] +
-    common_run_list +
-    [
-      'recipe[jrb_workstation::brew]',
-      'recipe[jrb_workstation::brew_cask]',
-      'recipe[jrb_workstation::osx_sudoers]'
-    ]
-)
+run_list 'recipe[jrb_workstation::default]'
