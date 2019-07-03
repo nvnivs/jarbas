5634ca13aeb7e3e3280a8ca65f08fcecd09ca1ef
n p <nvnivs@gmail.com>
2019-07-03 16:35:03 +0200

    Added RVM, rubies and gems

--- cookbooks/jrb_workstation/recipes/ruby.rb
+++ cookbooks/jrb_workstation/recipes/ruby.rb
@@ -7,3 +7,54 @@
 package 'ruby' do
   action :upgrade
 end
+
+# Required by RVM
+package 'gpg2' do
+  action :upgrade
+end
+
+execute 'install_rvm' do
+  command 'curl -sSL https://get.rvm.io | bash -s stable'
+  not_if 'which rvm'
+  user Homebrew.owner
+  environment ({ 'HOME' => ::Dir.home(Homebrew.owner), 'USER' => Homebrew.owner })
+  cwd ::Dir.home(Homebrew.owner)
+end
+
+rubies = [
+  'ruby-1.9.3-p551',
+  'ruby-2.2.5',
+  'ruby-2.5.5'
+]
+
+rubies.each do |version|
+  execute "install_ruby[#{version}]" do
+    command "rvm install #{version}"
+    not_if "rvm list | grep -E #{version}"
+    user Homebrew.owner
+    environment ({ 'HOME' => ::Dir.home(Homebrew.owner), 'USER' => Homebrew.owner })
+    cwd ::Dir.home(Homebrew.owner)
+  end
+end
+
+execute 'set_default_ruby' do
+  command 'rvm use 2.5.5 --default'
+  not_if 'rvm list | grep -E "\* ruby-2.5.5"'
+  user Homebrew.owner
+  environment ({ 'HOME' => ::Dir.home(Homebrew.owner), 'USER' => Homebrew.owner })
+  cwd ::Dir.home(Homebrew.owner)
+end
+
+gems = [
+  'encrypt_data_bag'
+]
+
+gems.each do |g|
+  execute "install_gem[#{g}]" do
+    command "gem install #{g} --no-rdoc --no-ri"
+    not_if "gem list | grep -E \"#{g}\""
+    user Homebrew.owner
+    environment ({ 'HOME' => ::Dir.home(Homebrew.owner), 'USER' => Homebrew.owner })
+    cwd ::Dir.home(Homebrew.owner)
+  end
+end
