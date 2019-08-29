37304749e501c7c82c6bf1e1c6b4bfd718156f46
n p <nvnivs@gmail.com>
2019-08-29 22:30:53 +0200

    Added check for brew installation when checking for outdated libraries

--- cookbooks/jrb_workstation/libraries/Brew.rb
+++ cookbooks/jrb_workstation/libraries/Brew.rb
@@ -8,7 +8,22 @@
 # single word that starts with a capital letter and then continues to use
 # camel-casing throughout the remainder of the name.
 #
+
+def which(cmd)
+  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+    exts.each { |ext|
+      exe = File.join(path, "#{cmd}#{ext}")
+      return exe if File.executable?(exe) && !File.directory?(exe)
+    }
+  end
+  return nil
+end
+
 def outdated_casks
+  # Return empty array if brew is not installed
+  if which('brew').nil? then return [] end
+
   outdated = Mixlib::ShellOut.new("brew cask outdated",
                                   :user => node['jrb_workstation']['user'],
                                   :environment => ({
