56a228da0d3b9a14f2744b60b7d7edcc28cac4cf
n p <nvnivs@gmail.com>
2019-09-03 14:05:30 +0200

    Fixed python

--- cookbooks/jrb_workstation/recipes/python.rb
+++ cookbooks/jrb_workstation/recipes/python.rb
@@ -12,14 +12,19 @@ package 'python3' do
   action :upgrade
 end
 
-execute 'download-pip' do
+execute 'download-pip3' do
   command 'curl -s https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py'
-  not_if 'which pip'
+  not_if 'which pip3'
   notifies :run, 'execute[install-pip]', :immediately
+  user node['jrb_workstation']['user']
+  environment ({
+    'HOME' => node['jrb_workstation']['home'],
+    'USER' => node['jrb_workstation']['user'] })
+  cwd node['jrb_workstation']['home']
 end
 
-execute 'install-pip' do
-  command 'python3 /tmp/get-pip.py --user'
+execute 'install-pip3' do
+  command 'python /tmp/get-pip.py --user'
   action :nothing
   user node['jrb_workstation']['user']
   environment ({
@@ -28,9 +33,9 @@ execute 'install-pip' do
   cwd node['jrb_workstation']['home']
 end
 
-execute 'install-awscli' do
-  command 'pip3 install awscli --upgrade --user'
-  not_if 'which aws'
+execute 'install-pip' do
+  command 'sudo easy_install pip'
+  not_if 'which pip'
   user node['jrb_workstation']['user']
   environment ({
     'HOME' => node['jrb_workstation']['home'],
@@ -38,12 +43,12 @@ execute 'install-awscli' do
   cwd node['jrb_workstation']['home']
 end
 
-execute 'install-powerline-shell' do
-  command 'pip3 install powerline-shell --upgrade --user'
+execute 'pip-install[awscli]' do
+  command 'pip install awscli --upgrade'
+  not_if 'which aws'
+end
+
+execute 'pip-install[powerline-shell]' do
+  command 'pip install powerline-shell --upgrade'
   not_if 'which powerline-shell'
-  user node['jrb_workstation']['user']
-  environment ({
-    'HOME' => node['jrb_workstation']['home'],
-    'USER' => node['jrb_workstation']['user'] })
-  cwd node['jrb_workstation']['home']
 end
