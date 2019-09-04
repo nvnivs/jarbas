3a12e9eb582f012b1d86165f38175447e05ad8c0
n p <nvnivs@gmail.com>
2019-09-04 15:08:32 +0200

    More osx settings

--- cookbooks/jrb_workstation/recipes/osx-settings.rb
+++ cookbooks/jrb_workstation/recipes/osx-settings.rb
@@ -8,3 +8,14 @@ jrb_workstation_execute 'disable_dashboard' do
   command 'defaults write com.apple.dashboard mcx-disabled -boolean YES && killall Dock'
   only_if 'ps ax |grep [D]ash'
 end
+
+jrb_workstation_execute 'set_dark_appearance' do
+  # killall Dock is not reloading
+  command 'defaults write NSGlobalDomain AppleInterfaceStyle Dark && killall Dock'
+  # guard is not working, executes all the time
+  not_if 'defaults read -g AppleInterfaceStyle |grep Dark'
+end
+
+jrb_workstation_execute 'finder_show_all_files' do
+  command 'defaults write com.apple.finder AppleShowAllFiles YES'
+end
