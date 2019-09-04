29ed11c6d2ba1c824d7c5cd932cdabd14cd46c65
n p <nvnivs@gmail.com>
2019-09-04 13:42:35 +0200

    Created wrapper resource to execute with user

--- cookbooks/jrb_workstation/metadata.rb
+++ cookbooks/jrb_workstation/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version '2.27.1'
+version '2.28.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 # issues_url 'https://github.com/<insert_org_here>/jrb_workstation/issues'
 # source_url 'https://github.com/<insert_org_here>/jrb_workstation'
