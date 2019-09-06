520a6b90b8e2c26f88e2d6e72b598ae8405cce29
n p <nvnivs@gmail.com>
2019-09-06 12:31:53 +0200

    Using vagrant cookbook

--- cookbooks/jrb_workstation/metadata.rb
+++ cookbooks/jrb_workstation/metadata.rb
@@ -4,10 +4,11 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version '2.32'
+version '2.33'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 # issues_url 'https://github.com/<insert_org_here>/jrb_workstation/issues'
 # source_url 'https://github.com/<insert_org_here>/jrb_workstation'
 
 depends 'homebrew'
 depends 'macos'
+depends 'vagrant'
--- cookbooks/jrb_workstation/recipes/vagrant.rb
+++ cookbooks/jrb_workstation/recipes/vagrant.rb
@@ -4,25 +4,12 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-# Homebrew is failing with URL
-#homebrew_cask 'vagrant' do
-#  cask_name 'https://raw.githubusercontent.com/caskroom/homebrew-cask/687a8a9145b055886413ed8edbb5632ee0830c8e/Casks/vagrant.rb'
-#end
-
 # Vagrant is failing on the version 2.2.5 due to issues with the plugins and the version of ruby
-execute 'install_vagrant' do
-  command "brew cask install https://raw.githubusercontent.com/caskroom/homebrew-cask/#{node['jrb_workstation']['vagrant']['version_sha']}/Casks/vagrant.rb"
-  not_if 'which vagrant'
-  user node['jrb_workstation']['user']
-  environment ({
-    'HOME' => node['jrb_workstation']['home'],
-    'USER' => node['jrb_workstation']['user'] })
-  cwd node['jrb_workstation']['home']
+vagrant 'install vagrant' do
+  version node['jrb_workstation']['vagrant']['version']
+  checksum node['jrb_workstation']['vagrant']['checksum']
 end
 
 node['jrb_workstation']['vagrant']['plugins'].each do |p|
-  execute "install_vagrant_plugin[#{p}]" do
-    command "vagrant plugin install #{p}"
-    not_if "vagrant plugin list |grep #{p}"
-  end
+  vagrant_plugin p
 end
