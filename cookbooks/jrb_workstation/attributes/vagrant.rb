c3dd6e6dfe943a71cc455c7c0b78dc8d0700f7f8
n p <nvnivs@gmail.com>
2019-09-06 13:10:35 +0200

    Cleaned up vagrant attributes

--- cookbooks/jrb_workstation/attributes/vagrant.rb
+++ cookbooks/jrb_workstation/attributes/vagrant.rb
@@ -1,10 +1,6 @@
 default['jrb_workstation']['vagrant']['version'] = '2.2.4'
 default['jrb_workstation']['vagrant']['checksum'] = 'a09a2f0fb3fcbd4cfb41c7812c01605173f64056449c3dbcc513b4211e9c1e8d'
 
-
-# Version for a brew cask version of vagrant
-default['jrb_workstation']['vagrant']['version_sha'] = '687a8a9145b055886413ed8edbb5632ee0830c8e'
-
 default['jrb_workstation']['vagrant']['plugins'] = [
   'vagrant-berkshelf',
   'vagrant-omnibus',
