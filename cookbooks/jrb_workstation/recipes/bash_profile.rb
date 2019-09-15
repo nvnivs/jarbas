454a512fd35151844ca8741da482c57af1d168bc
n p <nvnivs@gmail.com>
2019-09-15 15:49:49 +0100

    Bash profile

---
+++ cookbooks/jrb_workstation/recipes/bash_profile.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook:: jrb_workstation
+# Recipe:: bash_profile
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+file "#{node['jrb_workstation']['home']}/.profile" do
+  action :delete
+end
+
+directory "#{node['jrb_workstation']['home']}/bin" do
+  owner node['jrb_workstation']['user']
+  mode 0755
+end
+
+directory "#{node['jrb_workstation']['home']}/.alias" do
+  owner node['jrb_workstation']['user']
+  mode 0755
+end
+
+cookbook_file "#{node['jrb_workstation']['home']}/.bash_profile" do
+  source 'bash_profile'
+  owner node['jrb_workstation']['user']
+  mode 0644
+end
