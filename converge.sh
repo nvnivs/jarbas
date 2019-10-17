3c1683b40bf8ae3343a9d616fb11a71b549a8b71
n p <nvnivs@gmail.com>
2019-10-17 14:45:48 +0200

    Better deploy

--- converge.sh
+++ converge.sh
@@ -4,7 +4,7 @@ set -e
 recipe=${1:-default}
 runlist="recipe[jrb_workstation::osx-sudoers-nopass],recipe[jrb_workstation::${recipe}],recipe[jrb_workstation::osx-sudoers]"
 sudo -v
+if [ -f Berksfile.lock ]; then rm -rf Berksfile.lock; fi
 berks install
-berks update
 berks vendor
 sudo chef-client -z -o "${runlist}" -j .chef/node.json
