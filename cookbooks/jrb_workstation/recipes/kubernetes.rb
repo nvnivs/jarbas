864591a42b22efd605b2723a944e236ff769c413
n p <nvnivs@gmail.com>
2019-12-11 20:09:34 +0100

    Added minikube settings

--- cookbooks/jrb_workstation/recipes/kubernetes.rb
+++ cookbooks/jrb_workstation/recipes/kubernetes.rb
@@ -19,3 +19,23 @@ end
 package 'minikube' do
   action :upgrade
 end
+
+jrb_workstation_execute 'minikube[cpus]' do
+  command 'minikube config set cpus 4'
+  not_if 'minikube config get cpus |grep "^4$"'
+end
+
+#jrb_workstation_execute 'minikube[dashboard]' do
+#  command 'minikube config set dashboard true'
+#  not_if 'minikube config get dashboard |grep "^true$"'
+#end
+
+jrb_workstation_execute 'minikube[kubernetes-version]' do
+  command 'minikube config set kubernetes-version 1.15.0'
+  not_if 'minikube config get kubernetes-version |grep "^1\.15\.0$"'
+end
+
+jrb_workstation_execute 'minikube[memory]' do
+  command 'minikube config set memory 6048'
+  not_if 'minikube config get memory |grep "^6048$"'
+end
