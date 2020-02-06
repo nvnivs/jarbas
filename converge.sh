06218fde25ee1ba07cd350dc9725e737ae3d3241
n p <nvnivs@gmail.com>
2020-02-06 12:38:11 +0000

    Improvements to converge plus yamllint

--- converge.sh
+++ converge.sh
@@ -3,8 +3,9 @@
 set -e
 recipe=${1:-default}
 runlist="recipe[jrb_workstation::osx-sudoers-nopass],recipe[jrb_workstation::${recipe}],recipe[jrb_workstation::osx-sudoers]"
+json='{"jrb_workstation":{"home": "'"${HOME}"'", "user":"'"${USER}"'"}}'
 sudo -v
 if [ -f Berksfile.lock ]; then rm -rf Berksfile.lock; fi
 berks install
 berks vendor
-sudo chef-client -z -o "${runlist}" -j .chef/node.json
+echo ${json} | sudo chef-client -z -o "${runlist}" -j /dev/stdin
