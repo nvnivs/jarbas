36096e2999cda9695d00e956245b3b57c57cb982
n <nvnivs@gmail.com>
2020-04-05 23:04:42 +0000

    Major version bump

--- cookbooks/jrb_workstation/metadata.rb
+++ cookbooks/jrb_workstation/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version '2.58.0'
+version '3.0.0'
 hef_version '>= 12.14' if respond_to?(:chef_version)
 # issues_url 'https://github.com/<insert_org_here>/jrb_workstation/issues'
 # source_url 'https://github.com/<insert_org_here>/jrb_workstation'
