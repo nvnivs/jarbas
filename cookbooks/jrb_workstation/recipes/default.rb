dec7114464483f2d2be8ee15ed3fe6949bf530e7
n <nvnivs@gmail.com>
2020-04-05 22:43:52 +0000

    Vim first please

--- cookbooks/jrb_workstation/recipes/default.rb
+++ cookbooks/jrb_workstation/recipes/default.rb
@@ -4,13 +4,13 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
+include_recipe 'jrb_workstation::vim'
 include_recipe 'jrb_workstation::command-line-tools' if node['platform'] == 'mac_os_x'
 include_recipe 'jrb_workstation::brew' if node['platform'] == 'mac_os_x'
 include_recipe 'jrb_workstation::brew-cask' if node['platform'] == 'mac_os_x'
 include_recipe 'jrb_workstation::ruby'
 include_recipe 'jrb_workstation::python'
 include_recipe 'jrb_workstation::bash_profile'
-include_recipe 'jrb_workstation::vim'
 include_recipe 'jrb_workstation::powerline_fonts'
 include_recipe 'jrb_workstation::iterm'
 include_recipe 'jrb_workstation::zsh'
