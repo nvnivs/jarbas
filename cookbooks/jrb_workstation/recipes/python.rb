57722448d546a402960615e2b021343ccbc9b40b
n p <nvnivs@gmail.com>
2020-04-07 09:30:53 +0200

    Made python recipe mac friendly

--- cookbooks/jrb_workstation/recipes/python.rb
+++ cookbooks/jrb_workstation/recipes/python.rb
@@ -6,4 +6,13 @@
 #
 package 'python'
 package 'python3'
-package 'python-pip'
+
+package 'python-pip' do
+  not_if { node['platform'] == 'mac_os_x' }
+end
+
+jrb_workstation_execute 'install-pip' do
+  command 'sudo easy_install pip'
+  not_if  'which pip'
+  only_if { node['platform'] == 'mac_os_x' }
+end
