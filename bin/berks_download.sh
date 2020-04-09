a66f0b18bedf38fa938eec295e0daab2c3451d3d
n <nvnivs@gmail.com>
2020-04-09 23:07:12 +0000

    Merged berks update with converge
    Berks update is required to push current cookbook version to
    'berks-cookbooks'

--- bin/berks_download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pushd $(dirname $(dirname $(realpath $0)))
-if [ -f Berksfile.lock ]; then rm -rf Berksfile.lock; fi
-berks install
-berks vendor
-popd
--- bin/converge.sh
+++ bin/converge.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+set -e
+
+# Parameters
 recipe=${1:-default}
-pushd pushd $(dirname $(dirname $(realpath $0)))
+
+# Get ready
+sudo -v
+pushd $(dirname $(dirname $(realpath $0)))
+
+# Berks update
+if [ -f Berksfile.lock ]; then rm -rf Berksfile.lock; fi
+berks install
+berks vendor
+
+# Run chef
 runlist="recipe[jrb_workstation::osx_sudoers_nopass],recipe[jrb_workstation::${recipe}],recipe[jrb_workstation::osx_sudoers]"
 json='{"jrb_workstation":{"home": "'"${HOME}"'", "user":"'"${USER}"'"}}'
-sudo -v
 echo ${json} | sudo chef-client -z -o "${runlist}" -j /dev/stdin --chef-license accept
+
+# Tidy up
 popd
