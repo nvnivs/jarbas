e4e4c72dfd4c01a45d431c13d1cee1b514d3ed82
n <nvnivs@gmail.com>
2020-04-09 23:21:59 +0000

    Added termite

--- cookbooks/jrb_workstation/recipes/default.rb
+++ cookbooks/jrb_workstation/recipes/default.rb
@@ -18,7 +18,11 @@ include_recipe 'jrb_workstation::python'
 include_recipe 'jrb_workstation::aws'
 include_recipe 'jrb_workstation::bash_profile'
 include_recipe 'jrb_workstation::powerline_fonts'
+
+# Terminal
 include_recipe 'jrb_workstation::iterm' if node['platform'] == 'mac_os_x'
+include_recipe 'jrb_workstation::termite'if node['platform'] == 'arch'
+
 include_recipe 'jrb_workstation::vagrant' if node['platform'] == 'mac_os_x'
 include_recipe 'jrb_workstation::vscode' if node['platform'] == 'mac_os_x'
 include_recipe 'jrb_workstation::kubernetes' if node['platfrom'] == 'mac_os_x'
---
+++ cookbooks/jrb_workstation/recipes/termite.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: jrb_workstation
+# Recipe:: termite
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+package 'termite'
