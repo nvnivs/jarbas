a0667028221c2d1b8790782d6ce48033a94527ea
n <nvnivs@gmail.com>
2020-04-10 12:59:17 +0000

    Added arch patcher

---
+++ cookbooks/jrb_workstation/files/default/zsh_custom/patch_arch.zsh
@@ -0,0 +1,25 @@
+function patch_arch {
+  # Update
+  sudo pacman -Syu --noconfirm
+
+  # Clean
+  sudo pacman -Sc --noconfirm
+
+  # Check if reboot is required
+  NEXTLINE=0
+  FIND=""
+  for I in `file /boot/vmlinuz*`; do
+    if [ ${NEXTLINE} -eq 1 ]; then
+      FIND="${I}"
+      NEXTLINE=0
+    else
+      if [ "${I}" = "version" ]; then NEXTLINE=1; fi
+    fi
+  done
+  if [ ! "${FIND}" = "" ]; then
+    CURRENT_KERNEL=`uname -r`
+    if [ ! "${CURRENT_KERNEL}" = "${FIND}" ]; then
+      echo "!!! Kernel has been patched, a reboot is required !!!"
+    fi
+  fi
+}
