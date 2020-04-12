d7c82bde3fe466640d79b285a2193bfd275fe1ff
n <nvnivs@gmail.com>
2020-04-12 12:25:36 +0000

    Added termite configuration

--- cookbooks/jrb_workstation/recipes/termite.rb
+++ cookbooks/jrb_workstation/recipes/termite.rb
@@ -4,4 +4,21 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
+home = node['jrb_workstation']['home']
+
 package 'termite'
+
+directory "#{home}/.config/termite/" do
+  owner user
+  group user
+  mode  '0755'
+end
+
+template "#{home}/.config/termite/config" do
+  source 'termite_config.erb'
+  owner  user
+  group  user
+  mode   '0644'
+end
+
+
