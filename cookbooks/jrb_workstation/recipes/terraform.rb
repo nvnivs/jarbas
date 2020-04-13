b1d34cd3bc3b355e29404ffb295b8ec9c4f7ab65
n <nvnivs@gmail.com>
2020-04-13 21:06:06 +0000

    Changed action to upgrade

--- cookbooks/jrb_workstation/recipes/terraform.rb
+++ cookbooks/jrb_workstation/recipes/terraform.rb
@@ -5,6 +5,8 @@
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
 #TODO: version is getting flagrantly ignored
+# There's a terraform cookbook, might be better than this
 package 'terraform' do
+  action  :upgrade
   version '0.12.18'
 end
