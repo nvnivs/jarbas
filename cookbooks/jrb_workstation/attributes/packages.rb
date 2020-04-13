c0d60b047f90f0ddca88a6e33ee945ad0fb66400
n <nvnivs@gmail.com>
2020-04-13 22:39:34 +0000

    Added iftop and iotop

--- cookbooks/jrb_workstation/attributes/packages.rb
+++ cookbooks/jrb_workstation/attributes/packages.rb
@@ -1,6 +1,8 @@
 # List of default os packages to install or upgrade
 default['jrb_workstation']['packages']['upgrade'] = [
   'autossh',         # Automatically restart SSH sessions and tunnels
+  'iftop',           # Display bandwidth usage on an interface
+  'iotop',           # View I/O usage of processes
   'pass',            # Password manager
   'tree',            # Display directories as trees (with optional color/HTML output)
 ]
