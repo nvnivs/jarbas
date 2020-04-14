44862322318ebe4e4e726af36d522400860a6270
n p <nvnivs@gmail.com>
2020-04-14 12:20:33 +0200

    Fixed ordering

--- bin/converge.sh
+++ bin/converge.sh
@@ -6,6 +6,8 @@ set -e
 recipe=${1:-default}
 
 # Get ready
+runlist="recipe[jrb_workstation::osx_sudoers_nopass],recipe[jrb_workstation::${recipe}],recipe[jrb_workstation::osx_sudoers]"
+json='{"jrb_workstation":{"home": "'"${HOME}"'", "user":"'"${USER}"'"}}'
 sudo -v
 pushd $(dirname $(dirname $(realpath $0)))
 
@@ -15,8 +17,6 @@ berks install
 berks vendor
 
 # Run chef
-runlist="recipe[jrb_workstation::osx_sudoers_nopass],recipe[jrb_workstation::${recipe}],recipe[jrb_workstation::osx_sudoers]"
-json='{"jrb_workstation":{"home": "'"${HOME}"'", "user":"'"${USER}"'"}}'
 echo ${json} | sudo chef-client -z -o "${runlist}" -j /dev/stdin --chef-license accept
 
 # Tidy up
