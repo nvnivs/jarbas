44848546a07a40b5a0d92c1d7c55a50491a7000f
n p <nvnivs@gmail.com>
2020-04-14 12:56:52 +0200

    Added terraform

--- cookbooks/jrb_workstation/recipes/terraform.rb
+++ cookbooks/jrb_workstation/recipes/terraform.rb
@@ -4,9 +4,7 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
-#TODO: version is getting flagrantly ignored
-# There's a terraform cookbook, might be better than this
-package 'terraform' do
-  action  :upgrade
-  version '0.12.18'
-end
+package 'terraform'
+
+#TODO: This is failing on the mac
+#include_recipe 'terraform::default'
