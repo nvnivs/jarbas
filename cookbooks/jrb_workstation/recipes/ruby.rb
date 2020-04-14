a0f8a59cc89cc2b77f42a84ac07b830e4544676c
n <nvnivs@gmail.com>
2020-04-14 21:48:24 +0000

    Some fixes in the ruby recipe

--- cookbooks/jrb_workstation/recipes/ruby.rb
+++ cookbooks/jrb_workstation/recipes/ruby.rb
@@ -4,40 +4,41 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-rvm_path='~/.rvm/scripts/rvm'
+rvm_path = "#{node['jrb_workstation']['home']}/.rvm/scripts/rvm"
+user     = node['jrb_workstation']['user']
 
 package 'ruby'
 package 'gnupg'
 
+#TODO: Guard not working
 node['jrb_workstation']['ruby']['gpg_keys'].each do |key|
   jrb_workstation_execute "install_gpg_key[#{key}]" do
     command "gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys #{key}"
-    not_if  "gpg2 --list-keys | grep #{key}"
+    not_if  "gpg2 --list-keys | grep '#{key}'", :user => user
   end
 end
 
 jrb_workstation_execute 'install_rvm' do
   command 'curl -sSL https://get.rvm.io | bash -s stable'
-  not_if  "which rvm"
+  not_if  'which rvm', :user => user
 end
 
 node['jrb_workstation']['ruby']['rubies'].each do |version|
   jrb_workstation_execute "install_ruby[#{version}]" do
     command "source #{rvm_path}; rvm install #{version}"
-    not_if  "source #{rvm_path}; rvm list | grep -E #{version}"
+    not_if  "source #{rvm_path}; rvm list | grep -E #{version}", :user => user
   end
 end
 
-#TODO: Defaut is not setting
-jrb_workstation_execute 'set_default_ruby' do
-  command "#{rvm_path} --default #{node['jrb_workstation']['ruby']['default_version']}"
-  not_if  "#{rvm_path} list | grep -F '* #{node['jrb_workstation']['ruby']['default_version']}'"
+jrb_workstation_execute 'default_ruby' do
+  command "source #{rvm_path}; rvm alias create default #{node['jrb_workstation']['ruby']['default_version']}"
+  not_if  "source #{rvm_path}; rvm list | grep -F '* #{node['jrb_workstation']['ruby']['default_version']}'", :user => user
 end
 
-#TODO: Gems are not installing
+#TODO: Guard not working
 node['jrb_workstation']['ruby']['gems'].each do |g|
   jrb_workstation_execute "install_gem[#{g}]" do
-    command "gem install #{g}"
-    not_if  "gem list | grep -E \"#{g}\""
+    command           "gem install #{g}"
+    not_if            "gem list | grep -E '#{g}'", :user => user
   end
 end
--- cookbooks/jrb_workstation/resources/execute.rb
+++ cookbooks/jrb_workstation/resources/execute.rb
@@ -6,8 +6,6 @@ action :run do
     command new_resource.command
     user    node['jrb_workstation']['user']
     cwd     node['jrb_workstation']['home']
-    only_if new_resource.only_if
-    not_if  new_resource.not_if
     environment(
       'HOME' => node['jrb_workstation']['home'],
       'USER' => node['jrb_workstation']['user']
