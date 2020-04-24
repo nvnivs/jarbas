75bbed3e857f930e2ed7c95513354247fd4de992
n <nvnivs@gmail.com>
2020-04-24 23:09:27 +0000

    Fixed terraform and tfenv

--- cookbooks/jrb_workstation/attributes/terraform.rb
+++ cookbooks/jrb_workstation/attributes/terraform.rb
@@ -1,1 +1,6 @@
-default['jrb_workstation']['terraform']['version'] = '0.12.18'
+default['jrb_workstation']['terraform']['default_version'] = '0.12.24'
+
+default['jrb_workstation']['terraform']['versions'] = [
+  '0.12.24',
+  '0.12.18',
+]
--- cookbooks/jrb_workstation/recipes/terraform.rb
+++ cookbooks/jrb_workstation/recipes/terraform.rb
@@ -4,6 +4,26 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
-package 'terraform' do
-  version node.default['jrb_workstation']['terraform']['version']
+jrb_workstation_aur 'tfenv' do
+  action [ :build, :install ]
+end
+
+# User needs to reload a new session for groups to load
+group 'tfenv' do
+  action  [ :create, :modify ]
+  members node['jrb_workstation']['user']
+  append  true
+end
+
+node['jrb_workstation']['terraform']['versions'].each do |v|
+  execute "terraform[#{v}]" do
+    command "tfenv install #{v}"
+    not_if  "tfenv list |grep -e '#{v}'"
+  end
+end
+
+file '/var/lib/tfenv/version' do
+  content node['jrb_workstation']['terraform']['default_version']
+  group   'tfenv'
+  mode    0664
 end
