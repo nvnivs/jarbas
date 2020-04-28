4c71ab7c019023d6fe7c6c5bc487b24c5c79c249
z0c <nvnivs@gmail.com>
2020-04-28 18:24:19 +0200

    Fixed mac_os_x issues

--- cookbooks/jarbas/recipes/terraform.rb
+++ cookbooks/jarbas/recipes/terraform.rb
@@ -4,8 +4,15 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
+# Install tfenv package
 jarbas_aur 'tfenv' do
-  action [ :build, :install ]
+  action    [ :build, :install ]
+  only_if   { node['platform'] == 'arch' }
+end
+
+homebrew_package 'tfenv' do
+  action    :upgrade
+  only_if   { node['platform'] == 'mac_os_x' }
 end
 
 # User needs to reload a new session for groups to load
@@ -19,15 +26,31 @@ node['jarbas']['terraform']['versions'].each do |v|
   execute "terraform[#{v}]" do
     command "tfenv install #{v}"
     not_if  "tfenv list |grep -e '#{v}'"
+    # Setting working directory to /tmp otherwise mkdirtmp will try to create on root on mac_os_x
+    cwd     '/tmp'
   end
 end
 
-file '/var/lib/tfenv/version' do
+file 'version' do
   content node['jarbas']['terraform']['default_version']
   group   'tfenv'
   mode    0664
+  path    case node['platform']
+          when 'mac_os_x'
+            '/usr/local/Cellar/tfenv/1.0.2/version'
+          else
+            '/var/lib/fenv/version'
+          end
 end
 
+# Terragrunt
 jarbas_aur 'terragrunt' do
-  action [ :build, :install ]
+  action  [ :build, :install ]
+  only_if { node['platform'] == 'arch' }
+end
+
+homebrew_package 'terragrunt' do
+  action  :upgrade
+  options '--ignore-dependencies'
+  only_if { node['platform'] == 'mac_os_x' }
 end
