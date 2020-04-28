82d583703e443736d84591a33295fe6aeda94ae2
z0c <nvnivs@gmail.com>
2020-04-28 19:14:36 +0200

    mac_os_x fixes

--- cookbooks/jarbas/recipes/qutebrowser.rb
+++ cookbooks/jarbas/recipes/qutebrowser.rb
@@ -4,4 +4,10 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
-package 'qutebrowser'
+package 'qutebrowser' do
+  not_if { node['platform'] == 'mac_os_x' }
+end
+
+homebrew_cask 'qutebrowser' do
+  only_if { node['platform'] == 'mac_os_x' }
+end
