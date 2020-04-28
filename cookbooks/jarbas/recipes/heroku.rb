8ab53b683450d1af4fde7ca23d928eb8be914cfe
z0c <nvnivs@gmail.com>
2020-04-28 19:01:21 +0200

    mac_os_x fixed

--- cookbooks/jarbas/recipes/heroku.rb
+++ cookbooks/jarbas/recipes/heroku.rb
@@ -1,3 +1,9 @@
 jarbas_aur 'heroku-cli' do
   action [ :build, :install ]
+  only_if { node['platform'] == 'arch'}
+end
+
+homebrew_package "heroku" do
+  action  :upgrade
+  only_if { node['platform'] == 'mac_os_x' }
 end
