97de4140c32b0d2a60e59c05f56c73e85e4bb6a5
n <nvnivs@gmail.com>
2021-11-20 14:47:36 +0100

    fix(git): Supports Manjaro

--- cookbooks/jarbas/recipes/git.rb
+++ cookbooks/jarbas/recipes/git.rb
@@ -4,4 +4,21 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
-package 'git'
+#TODO: Package resource does not support Manjaro provider
+package 'git' do
+  not_if { node['platform'] == 'manjaro' }
+end
+
+pacman_package 'git' do
+  only_if { node['platform'] == 'manjaro' }
+end
+
+#TODO: git cookbook does not support Manjaro
+#TODO: Add guards
+jarbas_execute 'git_config[user.name]' do
+  command "git config --global user.name '#{node['jarbas']['git']['user']['name']}'"
+end
+
+jarbas_execute 'git_config[user.email]' do
+  command "git config --global user.email '#{node['jarbas']['git']['user']['email']}'"
+end
