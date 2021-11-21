0358b145b94bee718eed333fed615139d670dd7b
n <nvnivs@gmail.com>
2021-11-21 12:03:23 +0100

    fix(vscode): Supports Manjaro

--- cookbooks/jarbas/recipes/vscode.rb
+++ cookbooks/jarbas/recipes/vscode.rb
@@ -4,10 +4,18 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-homebrew_cask 'visual-studio-code'
+homebrew_cask 'visual-studio-code' do
+  only_if [ node['platform'] == 'mac_os_x' ]
+end
+
+execute 'yaypackage[vscode]' do
+  command 'yay -S --noconfirm --noprogressbar visual-studio-code-bin'
+  not_if  [ node['platform'] == 'mac_os_x' ]
+  not_if  'yay -Q |grep visual-studio-code-bin'
+end
 
 node['jarbas']['vscode']['extensions'].each do |e|
-  execute "install_vscode_extension[#{e}]" do
+  jarbas_execute "install_vscode_extension[#{e}]" do
     command "code --install-extension #{e}"
     not_if "code --list-extensions |grep #{e}"
   end
