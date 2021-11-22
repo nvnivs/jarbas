c1f83069734bad2a138956e124be9c101303e945
n <nvnivs@gmail.com>
2021-11-22 18:29:25 +0100

    chore(iterm): Removes iterm from default runlist

--- cookbooks/jarbas/recipes/default.rb
+++ cookbooks/jarbas/recipes/default.rb
@@ -17,9 +17,6 @@ include_recipe 'jarbas::packages'
 include_recipe 'jarbas::vscode'
 include_recipe 'jarbas::brave'
 
-# Terminal
-include_recipe 'jarbas::iterm' if node['platform'] == 'mac_os_x'
-
 # Browser
 include_recipe 'jarbas::qutebrowser'
 
