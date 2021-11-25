c73d989480fc091d4e17f6f0cf481d07389d54ef
Nuno Pereira <nvnivs@gmail.com>
2021-11-25 16:50:19 +0100

    fix(git): Removed git global configs

--- cookbooks/jarbas/recipes/git.rb
+++ cookbooks/jarbas/recipes/git.rb
@@ -6,12 +6,3 @@
 
 jarbas_package 'git'
 
-#TODO: git cookbook does not support Manjaro
-#TODO: Add guards
-jarbas_execute 'git_config[user.name]' do
-  command "git config --global user.name '#{node['jarbas']['git']['user']['name']}'"
-end
-
-jarbas_execute 'git_config[user.email]' do
-  command "git config --global user.email '#{node['jarbas']['git']['user']['email']}'"
-end
