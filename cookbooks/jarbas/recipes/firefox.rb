df3c4b0a8d283d666d37b2b79eebe8ce2709d119
Nuno Pereira <nvnivs@gmail.com>
2021-11-26 14:58:59 +0100

    fix(firefox): Supports mac_os_x

--- cookbooks/jarbas/recipes/firefox.rb
+++ cookbooks/jarbas/recipes/firefox.rb
@@ -3,4 +3,9 @@
 # Recipe:: firefox
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
-jarbas_package 'firefox-developer-edition'
+
+if node['platform'] == 'mac_os_x' then
+    homebrew_cask 'firefox-developer-edition'
+else
+    jarbas_package 'firefox-developer-edition'
+end
