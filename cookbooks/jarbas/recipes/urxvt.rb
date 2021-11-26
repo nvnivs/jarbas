952c4e22a9d20d400b1edf70f5a6e9cd4f544892
Nuno Pereira <nvnivs@gmail.com>
2021-11-26 16:52:32 +0100

    chore(urxvt): Adds comments on failed expectations

--- cookbooks/jarbas/recipes/urxvt.rb
+++ cookbooks/jarbas/recipes/urxvt.rb
@@ -1,1 +1,8 @@
 jarbas_package 'rxvt-unicode'
+
+#TODO: Not linking to spotlight
+#TODO: Not starting up on the mac
+#link '/Applications/urxvt' do
+#    to      '/usr/local/bin/urxvt'
+#    only_if { node['platform'] == 'mac_os_x' }
+#end
