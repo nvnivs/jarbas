a21fbde13af9b2984457c4e46e288f6902d4df21
Nuno Pereira <nvnivs@gmail.com>
2021-11-26 15:03:26 +0100

    fix(brave): Supports mac_os_x

--- cookbooks/jarbas/recipes/brave.rb
+++ cookbooks/jarbas/recipes/brave.rb
@@ -4,8 +4,10 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
-jarbas_yay_package 'brave-bin' do
-    not_if [ node['platform'] == 'mac_os_x' ]
+if node['platform'] == 'mac_os_x' then
+    homebrew_cask 'brave-browser'
+else
+    jarbas_yay_package 'brave-bin'
 end
 
 #TODO: Install extensions programatically
