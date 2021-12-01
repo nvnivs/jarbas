de514c688d291ce5705acc63e85f51b46d5b952f
n <nvnivs@gmail.com>
2021-12-01 09:37:09 +0100

    fix(arch_post): Fixes permissions issue updating AUR packages

--- cookbooks/jarbas/resources/package.rb
+++ cookbooks/jarbas/resources/package.rb
@@ -3,13 +3,9 @@ property :name, String, name_property: true
 action :install do
   case node['platform']
   when 'mac_os_x', 'arch'
-    package new_resource.name do
-      action :install
-    end
+    package new_resource.name
   when 'manjaro'
-    pacman_package new_resource.name do
-      action  :install
-    end 
+    pacman_package new_resource.name
   else
       raise 'Unsupported platform'
   end     
