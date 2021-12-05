761728961c85c777118082aa45921b77cd45a8d5
n <nvnivs@gmail.com>
2021-12-05 13:13:39 +0100

    feat(patching): Reboots if Kernel changed

--- cookbooks/jarbas/recipes/platform_arch_post.rb
+++ cookbooks/jarbas/recipes/platform_arch_post.rb
@@ -5,24 +5,29 @@
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
 execute 'Pacman update' do
-    command     'pacman -Syu --noconfirm'
-    live_stream true
+  command     'pacman -Syu --noconfirm'
+  live_stream true
 end
 
 execute 'Pacman clean' do
-    command     'pacman -Sc --noconfirm'
-    live_stream true
+  command     'pacman -Sc --noconfirm'
+  live_stream true
 end
 
 execute 'Yay update' do
-    command     'yay -Syu --builddir=/tmp/ --noconfirm'
-    live_stream true
+  command     'yay -Syu --builddir=/tmp/ --noconfirm'
+  live_stream true
 end
 
 execute 'Yay clean' do
-    command     'yay -Sc --noconfirm'
-    live_stream true
+  command     'yay -Sc --noconfirm'
+  live_stream true
 end
 
-#TODO: Reboot, play with this:
-# [[ $(pacman -Q linux | cut -d " " -f 2) > $(uname -r) ]] && echo reboot
+#TODO: guard needs to change if not manjaro
+reboot 'Kernel changed' do
+  action     :request_reboot
+  reason     'Kernel changed, rebooting...'
+  delay_mins 2
+  only_if    '$(pacman -Q linux |cut -d " " -f2) != $(uname -r |sed s/-MANJARO//g)'
+end
