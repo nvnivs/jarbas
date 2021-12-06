4535798e88de993fb4c308b88e22fd1baf039d07
n <nvnivs@gmail.com>
2021-12-06 12:23:46 +0100

    fix(spotify): Adds gpg key

--- cookbooks/jarbas/recipes/spotify.rb
+++ cookbooks/jarbas/recipes/spotify.rb
@@ -4,6 +4,13 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
+jarbas_execute 'install_gpg_key[931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90]' do
+  command 'gpg2 --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90'
+  not_if  'gpg2 -k 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90',
+          :user => node['jarbas']['user'],
+          :environment => { 'HOME' => node['jarbas']['home']}
+end
+
 case node['platform']
 when 'arch', 'manjaro'
   jarbas_yay_package 'spotify'
