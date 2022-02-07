4381a6831812e2c7b06adb088a4fc5ba46b2b537
n <nvnivs@gmail.com>
2022-02-07 14:44:54 +0100

    chore(yay): Removes deprecated comment

--- cookbooks/jarbas/recipes/yay.rb
+++ cookbooks/jarbas/recipes/yay.rb
@@ -4,7 +4,4 @@
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
-#HACK: Fix for kitchen failing to install yay if pacman db not up to date
-#      This shouldn't be needed here but `intermediate_instructions` in kitchen doesnt seem to be working
-
 jarbas_package 'yay'
