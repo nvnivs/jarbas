f4d75425bf1dc1a1b45cb4f5bdbe9d65da2987b7
n <nvnivs@gmail.com>
2022-02-07 13:01:00 +0100

    chore(yay): Adds commented beginings to build yay from source

--- cookbooks/jarbas/recipes/yay.rb
+++ cookbooks/jarbas/recipes/yay.rb
@@ -5,3 +5,33 @@
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
 jarbas_package 'yay'
+
+=begin
+
+# Build from source fails with the following error
+
+STDERR: ==> ERROR: Running makepkg as root is not allowed as it can cause permanent,
+catastrophic damage to your system.
+---- End output of makepkg -si ----
+Ran makepkg -si returned 10
+
+include_recipe 'jarbas::git'
+
+directory "#{node['jarbas']['repos']}" do
+  owner node['jarbas']['user']
+  mode  '0755'
+end
+
+git "#{node['jarbas']['repos']}/pfetch" do
+  repository  'https://github.com/Jguer/yay.git'
+  action      :checkout
+  user        node['jarbas']['user']
+  notifies    :run, 'jarbas_execute[build_yay]', :immediately
+end
+
+jarbas_execute 'build_yay' do
+  command 'makepkg -si'
+  cwd     node['jarbas']['repos']
+  action  :nothing
+end
+=end
--- cookbooks/jarbas/resources/execute.rb
+++ cookbooks/jarbas/resources/execute.rb
@@ -1,12 +1,13 @@
 property :command, String, required: true
 property :live_stream, [true, false], default: false
+property :cwd, String, required: false, default: node['jarbas']['home']
 unified_mode true
 
 action :run do
   execute   new_resource.name do
     command      new_resource.command
     user         node['jarbas']['user']
-    cwd          node['jarbas']['home']
+    cwd          new_resource.cwd
     live_stream  new_resource.live_stream
     environment(
       'HOME' => node['jarbas']['home'],
