2af397d8486c52dde43e32f013a67de868501df1
n <nvnivs@gmail.com>
2022-02-07 14:02:36 +0100

    fix(yay): Hackfixes yay not installing if pacman db not up to date

--- cookbooks/jarbas/recipes/yay.rb
+++ cookbooks/jarbas/recipes/yay.rb
@@ -4,6 +4,13 @@
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
+#HACK: Fix for kitchen failing to install yay if pacman db not up to date
+#      This shouldn't be needed here but `intermediate_instructions` in kitchen doesnt seem to be working
+execute 'Pacman update' do
+  command     'pacman -Syu --noconfirm'
+  live_stream true
+end
+
 jarbas_package 'yay'
 
 =begin
