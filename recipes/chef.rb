0e77de5f99582d47dd4c1bf04e51d70068d1df4a
n <nvnivs@gmail.com>
2022-02-16 21:35:17 +0100

    chore(cookstyle): Fixes cookstyle inspections

--- recipes/chef.rb
+++ recipes/chef.rb
@@ -7,7 +7,7 @@
 case node['platform']
 when 'arch', 'manjaro'
   jarbas_yay_package 'chef-dk'
-when 'mac_os_x'  
+when 'mac_os_x'
   chef_client_updater 'Install latest'
 else
   raise 'Unsupported platform'
