4ffd9e984fb32fe093b05bd41991ee1bf5bf4403
n <nvnivs@gmail.com>
2022-02-17 20:14:13 +0100

    fix(yay): Fixes identation

--- recipes/yay.rb
+++ recipes/yay.rb
@@ -5,9 +5,9 @@
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
 directory '/var/cache/yay' do
-    owner 'root'
-    group 'root'
-    mode  '0777'
+  owner 'root'
+  group 'root'
+  mode  '0777'
 end
 
 jarbas_package 'yay'
