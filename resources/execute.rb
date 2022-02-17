7d77bf49f5ca620aed68fe7e70b007833def1f9a
N P <nvnivs@gmail.com>
2022-02-17 11:31:39 +0100

    fix(execute): Removes unused cwd (#4)

--- resources/execute.rb
+++ resources/execute.rb
@@ -1,13 +1,12 @@
 property :command, String, required: true
 property :live_stream, [true, false], default: false
-property :cwd, String, required: false, default: node['jarbas']['home']
 unified_mode true
 
 action :run do
   execute   new_resource.name do
     command      new_resource.command
     user         node['jarbas']['user']
-    cwd          new_resource.cwd
+    cwd          node['jarbas']['home']
     live_stream  new_resource.live_stream
     environment(
       'HOME' => node['jarbas']['home'],
