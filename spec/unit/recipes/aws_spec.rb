577e44ab07899ca0b05b8678a33b30f3789d52da
N P <nvnivs@gmail.com>
2022-02-17 13:50:12 +0100

    chore(unittests): Clears all unit tests but for to server as a basis. (#5)

---
+++ spec/unit/recipes/aws_spec.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook:: jarbas
+# Spec:: aws
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'jarbas::aws' do
+  context 'When all attributes are default, on Arch' do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+    platform 'arch'
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
