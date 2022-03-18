09f2b98a914f228755847fa2b06ee80af82c9569
n <nvnivs@gmail.com>
2022-03-18 19:06:57 +0100

    fix(chef): Sets up chef-workstation on manjaro

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,9 @@ maintainer 'n'
 maintainer_email 'nvnivs@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures workstation'
-version '5.5.0'
+version '5.5.1'
 
-chef_version '>= 15.14'
+chef_version '>= 17.2'
 issues_url 'https://github.com/z0c/jarbas/issues'
 source_url 'https://github.com/z0c/jarbas'
 
