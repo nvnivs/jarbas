9cbedf3291c2d8d2e8746b96e4b85f0e0655a7a1
n <nvnivs@gmail.com>
2022-04-16 18:20:48 +0200

    fix(pacman): Fixes pacman update guard

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'n'
 maintainer_email 'nvnivs@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures workstation'
-version '5.5.2'
+version '5.5.3'
 
 chef_version '>= 17.2'
 issues_url 'https://github.com/z0c/jarbas/issues'
--- recipes/post_platform_arch.rb
+++ recipes/post_platform_arch.rb
@@ -9,21 +9,8 @@ execute 'Pacman clean' do
   live_stream true
 end
 
-execute 'Yay update' do
-  command     'yay -Syu --builddir=/tmp/ --noconfirm'
-  live_stream true
-end
-
 execute 'Yay clean' do
   command     'yay -Sc --noconfirm'
   live_stream true
   only_if     { ::File.directory?('/var/cache/yay') }
-end
-
-# TODO: guard needs to change if not manjaro
-reboot 'Kernel changed' do
-  action     :request_reboot
-  reason     'Kernel changed, rebooting...'
-  delay_mins 2
-  only_if    '$(pacman -Q linux |cut -d " " -f2) != $(uname -r |sed s/-MANJARO//g)'
 end
