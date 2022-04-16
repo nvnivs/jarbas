b827e160031798ac818c74cfa4a1249ba156942d
n <nvnivs@gmail.com>
2022-04-16 18:36:29 +0200

    Fixes yay update stub command

--- spec/unit/recipes/pre_platform_arch_spec.rb
+++ spec/unit/recipes/pre_platform_arch_spec.rb
@@ -10,7 +10,7 @@ describe 'jarbas::pre_platform_arch' do
   context 'when no packages need update' do
     it 'does not execute pacman update' do
       stub_command('checkupdates').and_return(false)
-      stub_command('(( $(yay -Qu |wc -l) > 0 ))').and_return(false)
+      stub_command('[[ $(yay -Qu |wc -l) == 0 ]]').and_return(false)
 
       expect(chef_run).not_to run_execute('pacman -Syyu --noconfirm')
       expect(chef_run).not_to run_execute('yay -Syu --builddir=/tmp/ --noconfirm')
@@ -20,7 +20,7 @@ describe 'jarbas::pre_platform_arch' do
   context 'when pacman needs update' do
     it 'executes pacman update' do
       stub_command('checkupdates').and_return(true)
-      stub_command('(( $(yay -Qu |wc -l) > 0 ))').and_return(false)
+      stub_command('[[ $(yay -Qu |wc -l) == 0 ]]').and_return(false)
 
       expect(chef_run).to run_execute('pacman -Syyu --noconfirm')
       expect(chef_run).not_to run_execute('yay -Syu --builddir=/tmp/ --noconfirm')
@@ -30,7 +30,7 @@ describe 'jarbas::pre_platform_arch' do
   context 'when yay needs update' do
     it 'executes yay update' do
       stub_command('checkupdates').and_return(false)
-      stub_command('(( $(yay -Qu |wc -l) > 0 ))').and_return(true)
+      stub_command('[[ $(yay -Qu |wc -l) == 0 ]]').and_return(true)
 
       expect(chef_run).not_to run_execute('pacman -Syyu --noconfirm')
       expect(chef_run).to run_execute('yay -Syu --builddir=/tmp/ --noconfirm')
