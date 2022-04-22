733e6ceaf6d4ebaaea16da7e45782a877d91073c
n <nvnivs@gmail.com>
2022-04-22 11:32:56 +0200

    fix(clean): Triggers pacman and yay clean on update

--- recipes/post_platform_arch.rb
+++ recipes/post_platform_arch.rb
@@ -3,14 +3,3 @@
 # Recipe:: post_platform_arch
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
-
-execute 'Pacman clean' do
-  command     'pacman -Sc --noconfirm'
-  live_stream true
-end
-
-execute 'Yay clean' do
-  command     'yay -Sc --noconfirm'
-  live_stream true
-  only_if     { ::File.directory?('/var/cache/yay') }
-end
