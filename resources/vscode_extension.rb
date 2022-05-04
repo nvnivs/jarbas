e7aa61cd305932c18974e4959a18961f3bb74633
n <nvnivs@gamil.com>
2022-05-04 14:44:01 +0200

    Adds guard to only install extensions if code is installed

--- resources/vscode_extension.rb
+++ resources/vscode_extension.rb
@@ -3,6 +3,12 @@ unified_mode true
 action :install do
   jarbas_execute "vscode_extension[#{new_resource.name}]" do
     command "code --install-extension #{new_resource.name}"
+    only_if 'which code',
+    user: node['jarbas']['user'],
+    environment: {
+      'HOME' => node['jarbas']['home'],
+      'USER' => node['jarbas']['user'],
+    }
     not_if  "code --list-extensions |grep #{new_resource.name}",
       user: node['jarbas']['user'],
       environment: {
