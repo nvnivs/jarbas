26ffd95c6c62f1bd8b554d4b707af9f7dabd5677
n <nvnivs@gamil.com>
2022-05-04 14:44:51 +0200

    Fixes identation

--- resources/vscode_extension.rb
+++ resources/vscode_extension.rb
@@ -4,10 +4,10 @@ action :install do
   jarbas_execute "vscode_extension[#{new_resource.name}]" do
     command "code --install-extension #{new_resource.name}"
     only_if 'which code',
-    user: node['jarbas']['user'],
-    environment: {
-      'HOME' => node['jarbas']['home'],
-      'USER' => node['jarbas']['user'],
+      user: node['jarbas']['user'],
+      environment: {
+        'HOME' => node['jarbas']['home'],
+        'USER' => node['jarbas']['user'],
     }
     not_if  "code --list-extensions |grep #{new_resource.name}",
       user: node['jarbas']['user'],
