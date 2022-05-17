843bf2e12831c4ebbe433093f2afa87cabf6baaa
Nuno Pereira <nvnivs@gmail.com>
2022-05-17 15:57:20 +0200

    Installs RDP client for Arch based platforms (#51)

--- recipes/rdp.rb
+++ recipes/rdp.rb
@@ -4,7 +4,11 @@
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
-if platform?('mac_os_x')
+case node['platform']
+when 'arch', 'manjaro'
+  jarbas_package 'freerdp'
+  jarbas_package 'remmina'
+when 'mac_os_x'
   homebrew_cask 'microsoft-remote-desktop'
 else
   raise 'Unsupported platform'
