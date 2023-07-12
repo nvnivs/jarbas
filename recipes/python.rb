e06573e8c8d57f8d748397d968a090f04e6d4d5f
N P <nvnivs@gmail.com>
2023-07-12 17:18:51 +0200

    fix(python): Changes installer for arch based systems

--- recipes/python.rb
+++ recipes/python.rb
@@ -6,37 +6,25 @@
 
 jarbas_package 'python'
 
-# pip
-case node['platform']
-when 'arch', 'manjaro'
-  jarbas_package 'python-pip'
-when 'mac_os_x'
-  # Skip, Homebrew installs pip3 pointing to the Homebrew’d Python 3 for you.
-  nil
-else
-  raise 'Unsupported platform'
-end
-
-# pytest
-case node['platform']
-when 'arch', 'manjaro'
-  jarbas_package 'python-pytest'
-else
-  raise 'Unsupported platform'
+# pip packages
+pip_packages = %w(pip pytest tox pre-commit)
+pip_packages.each do |p|
+  case node['platform']
+  when 'arch', 'manjaro'
+    jarbas_package "python-#{p}"
+  when 'mac_os_x'  
+    jarbas_execute "pip_package[#{p}]" do
+      command "pip3 install #{p}"
+      not_if  "pip3 list |grep #{p}",
+        user: node['jarbas']['user'],
+        environment: {
+          'HOME' => node['jarbas']['home'],
+          'USER' => node['jarbas']['user'],
+        }
+    end
+  else
+    raise 'Unsupported platform'
+  end
 end
 
 jarbas_vscode_extension 'ms-python.python'
-
-# pip packages
-pip_packages = %w(tox pre-commit)
-pip_packages.each do |p|
-  jarbas_execute "pip_package[#{p}]" do
-    command "pip3 install #{p}"
-    not_if  "pip3 list |grep #{p}",
-      user: node['jarbas']['user'],
-      environment: {
-        'HOME' => node['jarbas']['home'],
-        'USER' => node['jarbas']['user'],
-      }
-  end
-end
