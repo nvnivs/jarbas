031a2c598b4a449bb2be12e25055a67098e638d8
N P <nvnivs@gmail.com>
2023-11-01 10:58:23 +0100

    Deprecates checkov (#101)

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'n'
 maintainer_email 'nvnivs@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures workstation'
-version '5.16.1'
+version '5.17.0'
 
 chef_version '>= 17.2'
 issues_url 'https://github.com/z0c/jarbas/issues'
