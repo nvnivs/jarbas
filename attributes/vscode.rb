fdfdff0d3d788744365f1c6fc5a74f3a0cf7efbd
n <nvnivs@gmail.com>
2024-02-29 17:02:32 +0100

    Adds devcontainers

--- attributes/vscode.rb
+++ attributes/vscode.rb
@@ -3,6 +3,7 @@ default['jarbas']['vscode']['extensions'] = [
   'hashicorp.terraform',
   'ms-azuretools.vscode-docker',
   'ms-kubernetes-tools.vscode-kubernetes-tools',
+  'ms-vscode-remote.remote-containers',
   'golang.go',
   'rebornix.ruby',
   'redhat.vscode-yaml',
