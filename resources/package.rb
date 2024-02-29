b592d6bb409311687a9e7da7543f2ae19b8d9e4d
n <nvnivs@gmail.com>
2024-02-29 11:53:16 +0100

    Supports ubuntu

--- resources/package.rb
+++ resources/package.rb
@@ -2,7 +2,7 @@ unified_mode true
 
 action :install do
   case node['platform']
-  when 'mac_os_x', 'arch'
+  when 'mac_os_x', 'arch', 'ubuntu'
     package new_resource.name
   when 'manjaro'
     pacman_package new_resource.name
